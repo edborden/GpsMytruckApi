a1bbde7b5e0d72517b21c66ffc42dcbe820d415c
edborden <borden.edward@gmail.com>
2015-06-13 10:29:39 -0400

    PUT to Device for Device.Edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :message, only: :create
   resources :system_status, only: :index
   resources :companies, only: [:create,:update,:destroy]
-  resources :devices, only: [:create,:destroy] do
+  resources :devices, only: [:create,:update,:destroy] do
   	resources :locations, only: :index
   	resources :mileage, only: :index
   end
