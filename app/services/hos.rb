755fdb22d5a9531ff1d43a278bdc3ac472ee1e92
edborden <borden.edward@gmail.com>
2015-08-05 10:33:26 -0400

    hos new querystring

--- app/services/hos.rb
+++ app/services/hos.rb
@@ -8,22 +8,11 @@ class Hos
 
 	def post hardware_id,lat,lng,time,event_code,distance_traveled
 
-		query = {
-			p1: @user,
-			p2: @pass,
-			p3: @partner_id,
-			p4: hardware_id,
-			p5: time,
-			p6: lat,
-			p7: lng,
-			p8: event_code,
-			p9: event_name(event_code),
-			p10: distance_traveled
-		}
+		queryString = "?p1={#{@user}}&p2={#{@pass}}&p3={#{@partner_id}}&p4={#{hardware_id}}&p5={#{time}}&p6={#{lat}}&p7={#{lng}}&p8={#{event_code}}&p9={#{event_name(event_code)}}&p10={#{distance_traveled}}&p11={}&p12={}&p13={}"
 
-		response = HTTParty.get 'https://atsdriverdev.abw.com/atsrs/Svc1.svc/GPSReceive', {query: query}
-		puts response.request.raw_body
-		puts response.body,response.code, response.message, response.headers.inspect
+		response = HTTParty.get 'https://atsdriverdev.abw.com/atsrs/Svc1.svc/GPSReceive' + queryString
+
+		puts response.body
 
 	end
 
