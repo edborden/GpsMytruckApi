dcfcb03c9545a5d1962e1472b6118bd06d1241a7
edborden <borden.edward@gmail.com>
2015-08-05 10:55:54 -0400

    hos reformatting

--- app/services/hos.rb
+++ app/services/hos.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'cgi'
+
 class Hos
 
 	def initialize
@@ -8,11 +11,13 @@ class Hos
 
 	def post hardware_id,lat,lng,time,event_code,distance_traveled
 
-		queryString = "?p1={#{@user}}&p2={#{@pass}}&p3={#{@partner_id}}&p4={#{hardware_id}}&p5={#{time}}&p6={#{lat}}&p7={#{lng}}&p8={#{event_code}}&p9={#{event_name(event_code)}}&p10={#{distance_traveled}}&p11={}&p12={}&p13={}"
+		queryString = "?p1=" + CGI.escape("{#{@user}}") + "&p2=" + CGI.escape("{#{@pass}}") + "&p3=" + CGI.escape("{#{@partner_id}}") + "&p4=" + CGI.escape("{#{hardware_id}}") + "&p5=" + CGI.escape("{#{time}}") + "&p6=" + CGI.escape("{#{lat}}") + "&p7=" + CGI.escape("{#{lng}}") + "&p8=" + CGI.escape("{#{event_code}}") + "&p9=" + CGI.escape("{#{event_name(event_code)}}") + "&p10=" + CGI.escape("{#{distance_traveled}}") + "&p11=" + CGI.escape("{}") + "&p12=" + CGI.escape("{}") + "&p13=" + CGI.escape("{}")
 
-		response = HTTParty.get 'https://atsdriverdev.abw.com/atsrs/Svc1.svc/GPSReceive' + queryString
+		uri = URI.parse 'https://atsdriverdev.abw.com/atsrs/Svc1.svc/GPSReceive' + queryString
 
-		puts response.body
+		response = open uri
+
+		puts response.read()
 
 	end
 
