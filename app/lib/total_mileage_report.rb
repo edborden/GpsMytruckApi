66c42209679e0465efaea71208210b3d3ec373c7
edborden <borden.edward@gmail.com>
2015-08-08 22:14:46 -0400

    fix for location with no lat lng

--- app/lib/total_mileage_report.rb
+++ app/lib/total_mileage_report.rb
@@ -8,11 +8,13 @@ class TotalMileageReport
 		last_location = nil
 		total_meters = 0
 		@locations.find_each_with_order do |location|
-			if last_location
-				distance = LocationDistance.new(last_location,location).in_meters
-				total_meters += distance
+			if location.lat && location.lng
+				if last_location
+					distance = LocationDistance.new(last_location,location).in_meters
+					total_meters += distance
+				end
+				last_location = location
 			end
-			last_location = location
 		end
 		total_miles = GeoCalc::meters_to_miles(total_meters)
 		total_miles.round(2)
