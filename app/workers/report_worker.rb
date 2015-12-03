7e88ba2f45c9ee4f32048c0901acee73ac547271
edborden <borden.edward@gmail.com>
2015-12-02 23:32:31 -0500

    test

--- app/workers/report_worker.rb
+++ app/workers/report_worker.rb
@@ -1,3 +1,5 @@
+require 'iron_worker'
 require 'httparty'
 
-puts "test"
+response = HTTParty.get('http://twitter.com/statuses/public_timeline.json')
+puts response.body, response.code, response.message, response.headers.inspect
